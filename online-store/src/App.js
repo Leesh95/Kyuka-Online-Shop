442f65ccc5c960a5a743e2ed331c2339f9acf7a1
Lee <93089279+Leesh95@users.noreply.github.com>
2022-06-04 23:54:38 +0300

    Create-react-app

---
+++ online-store/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Kyuka</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;

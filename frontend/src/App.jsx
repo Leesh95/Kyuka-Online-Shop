43ae18e84daad24fd44c724e8f9f3c7f0eb27b0b
Lee <93089279+Leesh95@users.noreply.github.com>
2022-06-06 17:01:36 +0300

    sign-up-form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import { useContext } from 'react';
 import { Store } from './screens/Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import MessagesBar from './components/MessagesBar';
 
@@ -78,6 +79,7 @@ const App = () => {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
 
               <Route path="/" element={<HomeScreen />} />

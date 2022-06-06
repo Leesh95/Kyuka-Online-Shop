e91e339ed281c99d9c2e4fe97bd4f24a7f75c953
Lee <93089279+Leesh95@users.noreply.github.com>
2022-06-06 20:13:12 +0300

    images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,6 +86,7 @@ const App = () => {
             </Routes>
           </Container>
         </main>
+
         <footer>
           <div className="text-center">All rights reserved</div>
         </footer>
